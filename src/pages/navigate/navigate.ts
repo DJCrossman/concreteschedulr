6eb8992f08e3e0aa9b82f20f5202161c7836b85a
DJCrossman <david@crossman.io>
2016-12-30 23:59:44 -0600

    Added Google Maps native for iOS and Android

--- src/pages/navigate/navigate.ts
+++ src/pages/navigate/navigate.ts
@@ -1,14 +1,11 @@
 import {
  GoogleMap,
  GoogleMapsEvent,
- GoogleMapsLatLng,
- CameraPosition,
- GoogleMapsMarkerOptions,
- GoogleMapsMarker
+ GoogleMapsLatLng
 } from 'ionic-native';
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-navigate',
@@ -17,44 +14,41 @@ import { NavController } from 'ionic-angular';
 
 export class NavigatePage {
 
-    constructor(public navCtrl: NavController) { }
+    map: GoogleMap;
 
-    ngAfterViewInit() {
-        this.loadMap();
+    constructor(public navCtrl: NavController, public platform: Platform) {
+        platform.ready().then(() => {
+            if(platform.is('ios') || platform.is('android')) this.loadMap();
+        });
     }
 
     loadMap() {
-        // create a new map by passing HTMLElement
-        let element: HTMLElement = document.getElementById('map');
+        let location = new GoogleMapsLatLng(49.8900202,-97.1436709);
 
-        let map = new GoogleMap(element);
+        this.map = new GoogleMap('map', {
+          'backgroundColor': 'white',
+          'controls': {
+            'compass': true,
+            'myLocationButton': true,
+            'indoorPicker': true,
+            'zoom': true
+          },
+          'gestures': {
+            'scroll': true,
+            'tilt': true,
+            'rotate': true,
+            'zoom': true
+          },
+          'camera': {
+            'latLng': location,
+            'tilt': 30,
+            'zoom': 15,
+            'bearing': 50
+          }
+        });
 
-        // listen to MAP_READY event
-        map.one(GoogleMapsEvent.MAP_READY).then(() => console.log('Map is ready!'));
-
-        // create LatLng object
-        let ionic: GoogleMapsLatLng = new GoogleMapsLatLng(43.0741904,-89.3809802);
-
-        // create CameraPosition
-        let position: CameraPosition = {
-            target: ionic,
-            zoom: 18,
-            tilt: 30
-        };
-
-        // move the map's camera to position
-        map.moveCamera(position);
-
-        // create new marker
-        let markerOptions: GoogleMapsMarkerOptions = {
-            position: ionic,
-            title: 'Ionic'
-        };
-        map.addMarker(markerOptions)
-            .then((marker: GoogleMapsMarker) => {
-                marker.showInfoWindow();
-            }, ({error}) => {
-                console.log(error);
-            });
+        this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+            console.log('Map is ready!');
+        });
     }
 }
