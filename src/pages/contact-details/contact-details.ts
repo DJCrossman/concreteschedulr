2133a2a35dee66d3e01772e76eb47b1b0c2a0446
DJCrossman <david@crossman.io>
2016-12-31 19:21:01 -0600

    Added email and call functionality

--- src/pages/contact-details/contact-details.ts
+++ src/pages/contact-details/contact-details.ts
@@ -1,3 +1,4 @@
+import { CallNumber, EmailComposer } from 'ionic-native';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
@@ -11,6 +12,30 @@ export class ContactDetailsPage {
 
     constructor(private navCtrl: NavController, public navParams: NavParams) {
         this.contact = navParams.data;
+    }
+
+    onClickCallNumber(numberToCall) {
+        CallNumber.callNumber(numberToCall.replace(/\D/g,''), true)
+          .then(() => console.log('Launched dialer!'))
+          .catch(() => console.log('Error launching dialer'));
+    }
+
+    onClickEmailAddress(emailTo) {
+        EmailComposer.isAvailable()
+            .then((available: boolean) => { if(available) console.log('Email is available!'); })
+            .catch(() => console.log('Email unavailable.'));
+
+        let email = {
+            to: emailTo,
+            subject: 'GNCTR 2017: ',
+            body: '',
+            isHtml: true
+        };
+
+        // Send a text message using default options
+        EmailComposer.open(email)
+          .then(() => console.log('Launched email client!'))
+          .catch((e) => console.log('Error launching email client.\n' + JSON.stringify(e) + '\n'));
     }
 
 }
