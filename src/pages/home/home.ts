f365b51f3e96838cbdc85e535a2189660421172c
DJCrossman <david@crossman.io>
2017-01-02 14:16:29 -0600

    Grouped days to make it easier to view

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import * as Moment from 'moment';
+import * as moment from 'moment';
 import { NavController } from 'ionic-angular';
 import { CalendarList } from '../../app/constants/calendar';
 import { EventDetailsPage } from '../event-details/event-details';
@@ -12,14 +12,15 @@ import { EventDetailsPage } from '../event-details/event-details';
 export class HomePage {
 
     public calendarList: any[];
+    public groupedCalendarList: any[];
 
     constructor(public navCtrl: NavController) {
         CalendarList.forEach((i) => {
             Object.assign(
                 i,
                 {
-                    startDateFormatted: Moment(i.startDate).format('h:mma MMMM Do, YYYY'),
-                    endDateFormatted: Moment(i.endDate).format('h:mma MMMM Do, YYYY')
+                    startDateFormatted: moment(i.startDate).format('h:mma MMMM Do, YYYY'),
+                    endDateFormatted: moment(i.endDate).format('h:mma MMMM Do, YYYY')
                 }
             );
         });
@@ -33,10 +34,30 @@ export class HomePage {
             let bDate = new Date(a.endDate);
             return aDate < bDate;
         });
+        this.groupedCalendarList = this.groupEventsByDay(this.calendarList);
     }
 
     goToEventDetail(event) {
         this.navCtrl.push(EventDetailsPage, event);
+    }
+
+    groupEventsByDay(calendar) {
+        let eventGroupList = [];
+        let groupBy = (xs, key) => {
+            return xs.reduce((rv, x) => {
+                (rv[moment(x[key]).format('LL')] = rv[moment(x[key]).format('LL')] || []).push(x);
+                return rv;
+            }, {});
+        };
+        let groupedObject = groupBy(calendar, 'startDate');
+        Object.keys(groupedObject).forEach((k) => {
+            eventGroupList.push({label: k, group: groupedObject[k]});
+        });
+        return eventGroupList.sort((a,b) => {
+            let aDate = new Date(a.label);
+            let bDate = new Date(b.label);
+            return aDate < bDate ? -1 : (aDate > bDate ? 1 : 0);
+        });
     }
 
 }
